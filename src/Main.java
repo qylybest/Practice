110f40de437caa41557c81c1e259da5bc4bf8667
姚苏洋 <eric.yao@vipshop.com>
2017-02-25 14:11:13 +0800

    500. Keyboard Row

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by eric on 2017/2/23.
  */
@@ -7,6 +12,8 @@ public class Main {
         Main test = new Main();
 //        test.reverse("I am a coder");
 //        test.findComplement(5);
+//        String[] words = {"Hello", "Alaska", "Dad", "Peace"};
+//        System.out.println(test.findWords(words));
     }
 
     /**
@@ -40,5 +47,64 @@ public class Main {
         }
         System.out.println(num^((1<<(l+1))-1));
     }
+
+    /**
+     * 给定一组string，找出可以在string中所有字母均在键盘同一行的string
+     * @param words
+     */
+    public String[] findWords(String[] words) {
+        Map<Character,Integer> map = new HashMap<>();
+        map.put('q',1);map.put('w',1);map.put('e',1);map.put('r',1);map.put('t',1);
+        map.put('y',1);map.put('u',1);map.put('i',1);map.put('o',1);map.put('p',1);
+        map.put('Q',1);map.put('W',1);map.put('E',1);map.put('R',1);map.put('T',1);
+        map.put('Y',1);map.put('U',1);map.put('I',1);map.put('O',1);map.put('P',1);
+
+        map.put('a',2);map.put('s',2);map.put('d',2);map.put('f',2);map.put('g',2);
+        map.put('h',2);map.put('j',2);map.put('k',2);map.put('l',2);
+        map.put('A',2);map.put('S',2);map.put('D',2);map.put('F',2);map.put('G',2);
+        map.put('H',2);map.put('J',2);map.put('K',2);map.put('L',2);
+
+        map.put('z',3);map.put('x',3);map.put('c',3);map.put('v',3);map.put('b',3);
+        map.put('n',3);map.put('m',3);
+        map.put('Z',3);map.put('X',3);map.put('C',3);map.put('V',3);map.put('B',3);
+        map.put('N',3);map.put('M',3);
+
+        List<String> list = new ArrayList<>();
+
+        for(String word:words){
+            int l = word.length();
+            int k = map.get(word.charAt(0));
+            if( l == 1){//只有一个字符，符合
+                list.add(word);
+            }else{
+                int i = 1;
+                boolean inLine = true;
+                boolean e = true;
+                while(i<l){
+                    e = map.get(word.charAt(i))==k;
+                    if(!e){
+                        inLine = false;
+                        break;
+                    }
+                    i++;
+                }
+                if(inLine){
+                    list.add(word);
+                }
+            }
+        }
+
+        String[] strings = {};
+
+        int size = list.size();
+        if(size != 0){
+            strings = list.toArray(new String[size]);
+        }
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+        return strings;
+    }
+
 
 }
