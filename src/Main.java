712d415ed0027008c13bac10d09ffeb03a78e55f
姚苏洋 <eric.yao@vipshop.com>
2017-02-26 16:32:14 +0800

    For a web developer, it is very important to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
    1. The area of the rectangular web page you designed must equal to the given target area.
    
    2. The width W should not be larger than the length L, which means L >= W.
    
    3. The difference between length L and width W should be as small as possible.

--- src/Main.java
+++ src/Main.java
@@ -470,4 +470,35 @@ public class Main {
         return num==0?0:(num%9==0?9:(num%9));
     }
 
+    /**
+     * For a web developer, it is very important to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
+
+     1. The area of the rectangular web page you designed must equal to the given target area.
+
+     2. The width W should not be larger than the length L, which means L >= W.
+
+     3. The difference between length L and width W should be as small as possible.
+     * @param area
+     * @return
+     */
+    public int[] constructRectangle(int area) {
+        if(area<=0){
+            return new int[2];
+        }
+        double l = Math.sqrt(area);
+        int[] r = new int[2];
+        int i = (int)l;
+        while(i<=l && i>=1){
+            int k = area%i;
+            if(k == 0){
+                r[0] = area/i;
+                r[1] = i;
+                break;
+            }
+            i--;
+        }
+        return r;
+
+    }
+
 }
