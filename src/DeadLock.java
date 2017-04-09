8624c0ed73f25224807d084dfe58d6172c09dbb5
qylybest <qylybest@163.com>
2017-04-09 22:46:21 +0800

    multi processor deadlock

---
+++ src/DeadLock.java
@@ -0,0 +1,48 @@
+/**
+ * Created by yaosu on 2017/4/9 0009.
+ */
+public class DeadLock implements Runnable{
+	static Object o1 = new Object();
+	static Object o2 = new Object();
+
+    boolean flag = true;
+
+    DeadLock(Boolean flg){
+        this.flag = flg;
+    }
+
+	public void run(){
+		if(flag){
+			synchronized (o1){
+				try {
+					Thread.sleep(1000);
+					synchronized (o2){
+						System.out.println("获取到了o1和o2两个对象锁");
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}else{
+			synchronized (o2){
+				try {
+					Thread.sleep(1000);
+					synchronized (o1){
+						System.out.println("获取到了o2和o1两个对象锁");
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new DeadLock(true));
+        Thread t2 = new Thread(new DeadLock(false));
+
+        t1.start();
+        t2.start();
+    }
+}
--- src/ThreadPratice.java
+++ src/ThreadPratice.java
@@ -1,16 +1,19 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by eric on 2017/4/2.
  */
 public class ThreadPratice {
-    public static void main(String[] args) {
-        Prin p = new Prin();
-        new Thread(p).start();
-        new Thread(p).start();
-        new Thread(p).start();
-
+	public static void main(String[] args) {
+		Prin p = new Prin();
+		new Thread(p).start();
+		new Thread(p).start();
+		new Thread(p).start();
 //        new Prin2().start();
 //        new Prin2().start();
 //        new Prin2().start();
+        ExecutorService pool = Executors.newFixedThreadPool(2);
     }
 
 
@@ -19,31 +22,31 @@ public class ThreadPratice {
 }
 
 class Prin implements Runnable{
-    private int i = 10;
-    public void run(){
-        while(i>0){
+	private int i = 10;
+	public void run(){
+		while(i>0){
 //            try{
 //                Thread.sleep(200);
-                System.out.println("还剩"+i--);
+				System.out.println("还剩"+i--);
 //            }catch(InterruptedException e){
 
 //            }
-        }
-    }
+		}
+	}
 }
 
 class Prin2 extends Thread{
-    private int i = 10;
-    public void run(){
-        while(i>0){
-            try{
-                Thread.sleep(200);
-                System.out.println("还剩"+i--);
-            }catch(InterruptedException e){
+	private int i = 10;
+	public void run(){
+		while(i>0){
+			try{
+				Thread.sleep(200);
+				System.out.println("还剩"+i--);
+			}catch(InterruptedException e){
 
-            }
-        }
-    }
+			}
+		}
+	}
 }
 
 
