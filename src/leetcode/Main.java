9b630a1f8764e6ab698b1feedc6f8b8728c4ec47
姚苏洋 <eric.yao@vipshop.com>
2017-04-11 16:22:44 +0800

    /** 	 * Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
    	 The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
    
    	 You may assume that each input would have exactly one solution and you may not use the same element twice.
    
    	 Input: numbers={2, 7, 11, 15}, target=9
    	 Output: index1=1, index2=2
    
    	 */

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -597,5 +597,33 @@ public class Main {
 		return str;
 	}
 
+	/**
+	 * Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+	 The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+	 You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+	 Input: numbers={2, 7, 11, 15}, target=9
+	 Output: index1=1, index2=2
+
+	 */
+	public int[] twoSum(int[] numbers, int target) {
+	   int[] result = new int[2];
+	   //key 是target-当前，valud第一个下标index
+	   Map<Integer,Integer> map = new HashMap<>();
+
+	   for(int index = 0; index < numbers.length; index++){
+			if(map.get(target - numbers[index]) == null){
+				if(map.get(numbers[index]) == null){
+					map.put(numbers[index],index);
+				}
+			}else{
+				result[0] = map.get(target - numbers[index])+1;
+				result[1] = index + 1;
+			}
+	   }
+	   return result;
+	}
 
 }
